5df85c59921cdbf9f80dcb4f33b4aef5b65a9879
Kn0wn-Un <naikdarsh175@gmail.com>
2021-03-15 12:05:58 +0530

    working on ship

--- src/factories/ship/__tests__/ship.test.js
+++ src/factories/ship/__tests__/ship.test.js
@@ -1,5 +1,15 @@
 const ship = require('../ship');
 
+const ship1 = ship(2, { x: 1, y: 1 }, true);
+
 test('Ship test', () => {
-	expect(ship()).toBe('ship');
+	let det = ship1.getDetails();
+	let pos = ship1.findAllPos();
+	expect(det.length).toBe(2);
+	expect(det.startCoord).toEqual({ x: 1, y: 1 });
+	expect(det.isX).toBeTruthy();
+	expect(pos).toEqual([
+		[1, 1],
+		[1, 2],
+	]);
 });
--- src/factories/ship/ship.js
+++ src/factories/ship/ship.js
@@ -1,5 +1,19 @@
-function ship() {
-	return 'ship';
-}
+const ship = (length, startCoord, isX) => {
+	const getDetails = () => {
+		return { length, startCoord, isX };
+	};
+	const findAllPos = () => {
+		let pos = [];
+		if (isX) {
+			for (let i = startCoord.y; i <= length; i++)
+				pos.push([startCoord.x, i]);
+		} else {
+			for (let i = startCoord.x; i <= length; i++)
+				pos.push([i, startCoord.y]);
+		}
+		return pos;
+	};
+	return { getDetails, findAllPos };
+};
 
 module.exports = ship;
