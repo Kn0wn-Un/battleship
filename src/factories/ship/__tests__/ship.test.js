8410729e51f0df43011ef69d2b4d32378c39ea1c
Kn0wn-Un <naikdarsh175@gmail.com>
2021-03-16 11:34:56 +0530

    ship ff done

--- src/factories/ship/__tests__/ship.test.js
+++ src/factories/ship/__tests__/ship.test.js
@@ -2,14 +2,36 @@ const ship = require('../ship');
 
 const ship1 = ship(2, { x: 1, y: 1 }, true);
 
-test('Ship test', () => {
+test('Ship 1 test', () => {
 	let det = ship1.getDetails();
-	let pos = ship1.findAllPos();
 	expect(det.length).toBe(2);
 	expect(det.startCoord).toEqual({ x: 1, y: 1 });
-	expect(det.isX).toBeTruthy();
-	expect(pos).toEqual([
+	expect(det.isHorizontal).toBeTruthy();
+	expect(det.pos).toEqual([
 		[1, 1],
 		[1, 2],
 	]);
+	expect(ship1.hit({ x: 1, y: 2 })).toBe('hit');
+	expect(ship1.hit({ x: 1, y: 1 })).toBe('sunk');
+	expect(ship1.hit({ x: 0, y: 0 })).toBe('miss');
+});
+
+test('Ship 2 test', () => {
+	const ship2 = ship(4, { x: 0, y: 3 }, false);
+	let det = ship2.getDetails();
+	expect(det.length).toBe(4);
+	expect(det.startCoord).toEqual({ x: 0, y: 3 });
+	expect(det.isHorizontal).toBeFalsy();
+	expect(det.pos).toEqual([
+		[0, 3],
+		[1, 3],
+		[2, 3],
+		[3, 3],
+	]);
+	expect(ship2.hit({ x: 1, y: 1 })).toBe('miss');
+	expect(ship2.hit({ x: 2, y: 3 })).toBe('hit');
+	expect(ship2.hit({ x: 0, y: 3 })).toBe('hit');
+	expect(ship2.hit({ x: 0, y: 4 })).toBe('miss');
+	expect(ship2.hit({ x: 3, y: 3 })).toBe('hit');
+	expect(ship2.hit({ x: 1, y: 3 })).toBe('sunk');
 });
--- src/factories/ship/ship.js
+++ src/factories/ship/ship.js
@@ -1,19 +1,43 @@
-const ship = (length, startCoord, isX) => {
+const ship = (length, startCoord, isHorizontal) => {
+	const hitPos = [];
+	let hitCtr = 0;
 	const getDetails = () => {
-		return { length, startCoord, isX };
+		return { length, startCoord, isHorizontal, pos: findAllPos() };
 	};
 	const findAllPos = () => {
 		let pos = [];
-		if (isX) {
-			for (let i = startCoord.y; i <= length; i++)
+		if (isHorizontal) {
+			for (let i = startCoord.y, j = 1; i <= length; i++, j++)
 				pos.push([startCoord.x, i]);
 		} else {
-			for (let i = startCoord.x; i <= length; i++)
+			for (let i = startCoord.x, j = 1; j <= length; i++, j++)
 				pos.push([i, startCoord.y]);
 		}
 		return pos;
 	};
-	return { getDetails, findAllPos };
+	const hit = (coord) => {
+		const pos = findAllPos();
+		for (let i = 0; i < length; i++)
+			if (coord.x === pos[i][0] && coord.y === pos[i][1]) {
+				hitPos.push([coord.x, coord.y]);
+				hitCtr++;
+				if (hitCtr === length) return isSunk();
+				return 'hit';
+			}
+		return 'miss';
+	};
+	const isSunk = () => {
+		const pos = findAllPos();
+		hitPos.sort();
+		if (hitPos.length === pos.length) {
+			for (let i = 0; i < length; i++)
+				if (hitPos[i][0] !== pos[i][0] && hitPos[i][1] !== pos[i][0])
+					return 'err';
+			return 'sunk';
+		}
+		return 'err';
+	};
+	return { getDetails, findAllPos, hit };
 };
 
 module.exports = ship;
