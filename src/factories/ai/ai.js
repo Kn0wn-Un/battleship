502aed91d92aabb15342d6b19030b05100f4b872
Kn0wn-Un <naikdarsh175@gmail.com>
2021-04-12 12:50:50 +0530

    ai made better

--- src/factories/ai/ai.js
+++ src/factories/ai/ai.js
@@ -1,13 +1,57 @@
 const ai = (comp) => {
+	let nextShots = [];
+	let hits = [];
 	const play = () => {
 		while (true) {
-			const coords = {};
-			coords.x = Math.floor(Math.random() * 10);
-			coords.y = Math.floor(Math.random() * 10);
+			const coords =
+				nextShots.length === 0 ? randShots() : playNextShots();
 			const res = comp.play(coords);
 			if (/duplicate shot/.test(res) || res === 'err') continue;
+			if (/sunk/.test(res)) {
+				nextShots = [];
+				hits = [];
+			}
+			if (/hit/.test(res)) {
+				hits.push(coords);
+				secondShot(coords);
+			}
 			return res;
 		}
+	};
+	const randShots = () => {
+		const c = {};
+		c.x = Math.floor(Math.random() * 10);
+		c.y = Math.floor(Math.random() * 10);
+		return c;
+	};
+	const secondShot = (c) => {
+		if (c.x < 9) nextShots.push({ x: c.x + 1, y: c.y });
+		if (c.x > 0) nextShots.push({ x: c.x - 1, y: c.y });
+		if (c.y < 9) nextShots.push({ x: c.x, y: c.y + 1 });
+		if (c.y > 0) nextShots.push({ x: c.x, y: c.y - 1 });
+		if (hits.length > 1) {
+			const last = hits[hits.length - 1];
+			const prevL = hits[hits.length - 2];
+			if (Math.abs(last.x - prevL.x) === 1) removeY(last.y);
+			if (Math.abs(last.y - prevL.y) === 1) removeX(last.x);
+		}
+	};
+	const removeY = (y) => {
+		for (let i = 0; i < nextShots.length; i++) {
+			let c = nextShots[i];
+			if (c.y !== y) nextShots.splice(i, 1);
+		}
+	};
+	const removeX = (x) => {
+		for (let i = 0; i < nextShots.length; i++) {
+			let c = nextShots[i];
+			if (c.x !== x) nextShots.splice(i, 1);
+		}
+	};
+	const playNextShots = () => {
+		const s = nextShots.pop();
+
+		return s;
 	};
 	return { play };
 };
